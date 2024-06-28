a481c3a721febc6ff432bef36e2bac00381cde59
nickstershovel <116566824+nickstershovel@users.noreply.github.com>
2024-06-28 10:46:12 +0200

    Remove unused files

--- common/components/CommonComponent.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-      <p>This is a common component shared between brand_a and brand_b</p>
-    </div>
-  </template>
--- common/pages/about.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div>
-        <h1>About Us</h1>
-        <p>This is a common page shared between brands</p>
-    </div>
-</template>
